075337c423ead25552093fe5bcb83c7966510744
JUnior <jrcjuniorcesar@gmail.com>
2018-12-13 21:22:52 -0200

    Ambiente virtual

---
+++ comandos ambiente virtual.tcl
@@ -0,0 +1,21 @@
+criar ambiente virtual
+comando -> virtualenv nome do ambiente -p python
+
+Entrar no ambiente virtual a criado
+workon nome do ambiente
+
+Sair do ambiente virtual
+deactivate 
+
+Listar ambientes existentes
+c:/users/python/envs
+
+
+install jango
+
+pip install django
+django-admin startproject nome
+cd nome
+python mange.py startapp polls
+cd polls
+
